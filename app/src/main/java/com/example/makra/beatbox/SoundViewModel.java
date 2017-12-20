babe092086fbfe11995ad27d2320afbbf994b48f
makra <makranczi.d@gmail.com>
2017-12-20 17:26:11 +0100

    Initial commit Chapter 20

---
+++ app/src/main/java/com/example/makra/beatbox/SoundViewModel.java
@@ -0,0 +1,31 @@
+package com.example.makra.beatbox;
+
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+/**
+ * Created by makra on 2017. 12. 20..
+ */
+
+public class SoundViewModel extends BaseObservable {
+    private Sound mSound;
+    private BeatBox mBeatBox;
+
+    public SoundViewModel(BeatBox beatBox) {
+        mBeatBox = beatBox;
+    }
+
+    @Bindable
+    public String getTitle() {
+        return mSound.getName();
+    }
+
+    public Sound getSound() {
+        return mSound;
+    }
+
+    public void setSound(Sound sound) {
+        mSound = sound;
+        notifyChange();
+    }
+}
